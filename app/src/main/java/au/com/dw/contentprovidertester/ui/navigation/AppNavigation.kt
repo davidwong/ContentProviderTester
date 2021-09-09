eb2f9e70b05075c288d9881c777ccf8ec5df1d48
David Wong <developer@davidwong.com.au>
2021-09-10 09:06:42 +1000

    Add files for compose theming.

--- app/src/main/java/au/com/dw/contentprovidertester/ui/navigation/AppNavigation.kt
+++ app/src/main/java/au/com/dw/contentprovidertester/ui/navigation/AppNavigation.kt
@@ -5,6 +5,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import au.com.dw.contentprovidertester.theme.AppTheme
 import au.com.dw.contentprovidertester.ui.QueryViewModel
 import au.com.dw.contentprovidertester.ui.query.QueryFormOnlyScreen
 import au.com.dw.contentprovidertester.ui.query.QueryScreen
@@ -14,37 +15,52 @@ import au.com.dw.contentprovidertester.ui.unEscapeUriString
 
 @Composable
 fun AppNavigation() {
-    val navController = rememberNavController()
-    val vm: QueryViewModel = hiltViewModel()
-    // choose Query or QueryFormOnly as start destination
-    NavHost(navController, startDestination = Screen.Query.route) {
-        composable(route = Screen.Query.route) {
-            QueryScreen(vm, navController)
-        }
-        composable(route = Screen.Result.route) {
-            ResultScreen(vm) {
-                // reset UI state to clear data if go back to query screen for another query
-                vm.reset()
-                navController.navigateUp()
+    AppTheme {
+        val navController = rememberNavController()
+        val vm: QueryViewModel = hiltViewModel()
+        // choose Query or QueryFormOnly as start destination
+        NavHost(navController, startDestination = Screen.Query.route) {
+            composable(route = Screen.Query.route) {
+                QueryScreen(vm, navController)
             }
-        }
-        composable(route = Screen.QueryFormOnly.route) {
-            QueryFormOnlyScreen() {
-                    uri, projection, selection, selectionArgs, sortOrder ->
-                navController.navigate(Screen.ResultInvokeQuery.routeWithParams(uri, projection, selection, selectionArgs, sortOrder))
+            composable(route = Screen.Result.route) {
+                ResultScreen(vm) {
+                    // reset UI state to clear data if go back to query screen for another query
+                    vm.reset()
+                    navController.navigateUp()
+                }
             }
-        }
-        // don't need add list of arguments to composable parameters, since they are all default strings
-        composable(route = Screen.ResultInvokeQuery.route) { backStackEntry ->
-            val uri = backStackEntry.arguments?.getString("uri")
-            requireNotNull(uri) { "URI not found for query"}
-            val projection = backStackEntry.arguments?.getString("projection")
-            val selection = backStackEntry.arguments?.getString("selection")
-            val selectionArgs = backStackEntry.arguments?.getString("selectionArgs")
-            val sortOrder = backStackEntry.arguments?.getString("sortOrder")
-            // unescaping URI may not actually be necessary, as seems to be done already
-            val queryHolder = QueryHolder(unEscapeUriString(uri), projection, selection, selectionArgs, sortOrder)
-            ResultScreenInvokeQuery(queryHolder){ navController.navigateUp() }
+            composable(route = Screen.QueryFormOnly.route) {
+                QueryFormOnlyScreen() { uri, projection, selection, selectionArgs, sortOrder ->
+                    navController.navigate(
+                        Screen.ResultInvokeQuery.routeWithParams(
+                            uri,
+                            projection,
+                            selection,
+                            selectionArgs,
+                            sortOrder
+                        )
+                    )
+                }
+            }
+            // don't need add list of arguments to composable parameters, since they are all default strings
+            composable(route = Screen.ResultInvokeQuery.route) { backStackEntry ->
+                val uri = backStackEntry.arguments?.getString("uri")
+                requireNotNull(uri) { "URI not found for query" }
+                val projection = backStackEntry.arguments?.getString("projection")
+                val selection = backStackEntry.arguments?.getString("selection")
+                val selectionArgs = backStackEntry.arguments?.getString("selectionArgs")
+                val sortOrder = backStackEntry.arguments?.getString("sortOrder")
+                // unescaping URI may not actually be necessary, as seems to be done already
+                val queryHolder = QueryHolder(
+                    unEscapeUriString(uri),
+                    projection,
+                    selection,
+                    selectionArgs,
+                    sortOrder
+                )
+                ResultScreenInvokeQuery(queryHolder) { navController.navigateUp() }
+            }
         }
     }
 }
